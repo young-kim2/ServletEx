3b4d75a28831c452fd7ffd7c06eb0f2c9cd096b0
김영 <dhehd215@daum.net>
2021-08-19 16:12:51 +0900

    로그인 로그아웃 구현

--- ServerEx/WebContent/WEB-INF/views/includes/nav.jsp
+++ ServerEx/WebContent/WEB-INF/views/includes/nav.jsp
@@ -1,7 +1,17 @@
+<%@page import="com.bit.servlet.dao.UserVo" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%
+    //사용자 정보 확인
+    UserVo authUser=(UserVo)session.getAttribute("authUser");
+    %>
 <!-- Navigation -->
 <ul>
+<% if(authUser==null){ %>
 <li><a href="<%= request.getContextPath() %>/users?a=loginform">로그인</a></li>
 <li><a href="<%= request.getContextPath() %>/users?a=joinform">회원가입</a></li>
+<% } else{ %>
+<li><%= authUser.getName() %>님 환영합니다.</li>
+<li><a href="<%= request.getContextPath() %>/users?a=logout">로그아웃</a></li>
+<% } %>
 </ul>
--- ServerEx/src/com/bit/servlet/controller/UserServlet.java
+++ ServerEx/src/com/bit/servlet/controller/UserServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.bit.servlet.dao.UserDao;
 import com.bit.servlet.dao.UserDaoOrclImpl;
@@ -34,6 +35,14 @@ public class UserServlet extends HttpServlet {
 			//a=joinsuccess->가입 성공 화면으로 FORWARD
 			RequestDispatcher rd= req.getRequestDispatcher("WEB-INF/views/users/joinsuccess.jsp");
 			rd.forward(req,resp);
+		}else if("logout".equals(action)) {
+			//a=logout->세션을 무효화하고 ->/로 리다이렉트
+			HttpSession session=req.getSession();
+			//session.removeAttribute("authUser");
+			//개별 속성 삭제
+			session.invalidate();
+			//세션 전체를 무효화
+			resp.sendRedirect(req.getContextPath()+"/");
 		}else {
 			//처리할 수 없는 요청->404 에러
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
@@ -70,6 +79,27 @@ public class UserServlet extends HttpServlet {
 				RequestDispatcher rd=req.getRequestDispatcher("/WEB-INF/views/users/joinform.jsp");
 				
 				rd.forward(req, resp);
+			}
+		}else if("login".equals(action)) {
+			//a=login
+			UserDao dao=new UserDaoOrclImpl();
+			//파라미터
+			String email=req.getParameter("email");
+			String password=req.getParameter("password");
+			//사용자 검색
+			UserVo vo=dao.getUserByEmailAndPassword(email, password);
+			
+			if(vo != null) {
+				//사용자를 찾음
+				//세션 객체 확보
+				HttpSession session=req.getSession(true);
+				//세션 객체에 사용자 정보를 속성으로 추가
+				session.setAttribute("authUser", vo);
+				//홈페이지로 리다이렉트
+				resp.sendRedirect(req.getContextPath()+"/");
+			}else {
+				//그런 사용자 없음->로그인 폼으로 리다이렉트
+				resp.sendRedirect(req.getContextPath()+"/users?a=loginform");
 			}
 		}else {
 			//에러 페이지 전송
